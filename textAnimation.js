3a975741931c6fd5fc0e836b69742767c5c5b305
Jaime Rivas <jaimerivas81@outlook.com>
2020-10-14 17:53:19 -0700

    WIP H2 animation using JS

---
+++ textAnimation.js
@@ -0,0 +1,7 @@
+// Code Reference from CodePen: https://codepen.io/onge/pen/ByjNbj
+
+// H2 animation
+// Create the letter animation style using strings
+// Select all H2 elements
+// For each one, grab the text and wrap each character inside a span element
+// Then iterate through all the span elements and apply the animation
